d90d721ff02f669ba71c3852279ac8e5beb5de4b
efive-dev <edoardo.quinto.dev@gmail.com>
2025-09-01 10:44:52 +0200

    refactored code organization

--- src/main/kotlin/grids/03.kt
+++ src/main/kotlin/grids/03.kt
@@ -2,46 +2,31 @@ package grids
 
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
-import org.openrndr.extensions.Screenshots
-import org.openrndr.math.Vector2
-import org.openrndr.math.map
-import org.openrndr.shape.LineSegment
+import org.openrndr.extra.noise.Random
+import org.openrndr.extra.shapes.grid
 
 fun main() = application {
     configure {
         width = 900
         height = 450
     }
-
     program {
-        extend(Screenshots()) {
-            key = "s"
-            folder = "screenshots/"
-            async = false
-        }
-
-        val lineSegments = mutableListOf<LineSegment>()
-        val target = Vector2(0.0, 0.0)
-        val spacing = 100.0
-
-        for (x in 0..6) {
-            for (y in 0..3) {
-                val pos = Vector2(
-                    map(0.0, 6.0, 100.0, width.toDouble(), x.toDouble()),
-                    map(0.0, 3.0, 100.0, height.toDouble(), y.toDouble())
-                )
-                val dir = (target - pos).normalized
-                val end = target - dir * spacing
-                lineSegments.add(LineSegment(pos, end))
-            }
-        }
-
+        val colors = listOf(ColorRGBa.PINK, ColorRGBa.WHITE,
+            ColorRGBa.WHITE, ColorRGBa.PINK.shade(0.5))
+        val cells = drawer.bounds.grid(16, 6, 100.0, 100.0).flatten()
         extend {
-            drawer.clear(ColorRGBa.BLACK)
-            drawer.stroke = ColorRGBa.WHITE
-            drawer.strokeWeight = 1.0
-
-            drawer.lineSegments(lineSegments)
+            Random.resetState()
+            drawer.clear(ColorRGBa.WHITE)
+            cells.forEach { cell ->
+                val brightness = Random.simplex(cell.center * 0.0035) + 0.5
+                drawer.stroke = ColorRGBa.PINK.opacify(0.4)
+                drawer.fill = ColorRGBa.PINK.shade(brightness)
+                drawer.circle(cell.center, cell.width * 0.4)
+                if (Random.bool()) {
+                    drawer.fill = Random.pick(colors)
+                    drawer.circle(cell.center, cell.width * 0.2)
+                }
+            }
         }
     }
 }
